ea1ddb1a2d20b90b50e8ff8e835acb520722d73e
Hugo Morvan <hugo.morvan@aol.com>
2026-01-29 21:19:00 +0100

    2025 day 3 solved

--- 2025/day3/main.go
+++ 2025/day3/main.go
@@ -2,21 +2,77 @@ package main
 
 import (
 	"fmt"
-
+	"strconv"
 	"github.com/hugo-morvan/aoc/utils"
 )
 
 func main() {
 	lines := utils.ReadLines(utils.InputFile())
 
-	fmt.Println("Part 1:", solvePart1(lines))
-	fmt.Println("Part 2:", solvePart2(lines))
+	fmt.Println("Part 1:", solvePartOne(lines))
+	fmt.Println("Part 2:", solvePartTwo(lines))
 }
 
-func solvePart1(lines []string) int {
-	return 0
+func solvePartOne(lines []string) int {
+	total := 0
+	for _, line := range lines {
+		n := len(line)
+		best := -1
+		maxRight := -1
+
+		for i := n-1; i >= 0; i-- {
+			d := int(line[i] - '0')
+
+			if maxRight != -1 {
+				value := d*10 + maxRight
+				if value > best {
+					best = value
+				}
+			}
+
+			if d > maxRight {
+				maxRight = d
+			}
+		}
+		if best == -1 {
+			total += 0
+		}
+		total += best
+	}
+
+
+	return total
 }
 
-func solvePart2(lines []string) int {
-	return 0
+func solvePartTwo(lines []string) int {
+	total := 0 
+	// Greedy monotonic stack
+	for _, line := range lines {
+		k := 12
+		n := len(line)
+
+		if n < k {
+			return 69
+		}
+
+		drop := n-k
+		stack := make([]byte, 0, n)
+
+		for i := 0; i<n; i++ {
+			d := line[i]
+
+			for drop > 0 && len(stack) > 0 && stack[len(stack)-1] < d {
+				stack = stack[:len(stack)-1]
+				drop--
+
+			}
+
+			stack = append(stack, d)
+		}
+		dig, _ := strconv.Atoi(string(stack[:k]))
+		total += dig
+	}
+
+
+	return total
 }
